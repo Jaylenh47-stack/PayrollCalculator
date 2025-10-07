560b8f91fbfb6cb9b87860ea27fdc395dcbfc831
Jaylenh47-stack <jaylenh47@gmail.com>
2025-10-07 14:27:37 -0400

    complete

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,66 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeId;
+    private String name;
+    private float hoursWorked;
+    private double payRate;
+
+    public Employee(int employeeId, String name, float hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(float payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay(){
+       if (hoursWorked > 40) {
+           return (40 * payRate) + ((hoursWorked - 40)) * payRate * 1.5;
+       }
+       else {
+           return hoursWorked * payRate;
+       }
+    }
+
+
+
+
+
 }
+
+
+
+
+
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,45 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    private static void main(String[] args){
+
+    public static void main(String[] args){
 
 
+        try{
+            FileReader fileReader = new FileReader("sample.txt");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            String input;
+            while((input = bufReader.readLine()) != null){
+            Employee emp = makeEmployeeFromCode(input);
+            displayEmployee(emp);
+            }
+            bufReader.close();
+
+        }catch (Exception e) {
+            System.out.println("There was a file error");
+        }
+
+
+
+    }
+
+    public static void displayEmployee(Employee e){
+        System.out.printf("%d %s worked %.1f hours for a total of %.2f\n", e.getEmployeeId(), e.getName(), e.getPayRate(), e.getGrossPay());
+    }
+
+    public static Employee makeEmployeeFromCode(String code){
+        String [] split = code.split("\\|");
+        int employeeId= Integer.parseInt(split[0]);
+        String name= split[1];
+        float hoursWorked= Float.parseFloat(split[2]);
+        double payRate = Double.parseDouble(split[3]);
+
+        return new Employee(employeeId, name, hoursWorked, payRate);
     }
 }
